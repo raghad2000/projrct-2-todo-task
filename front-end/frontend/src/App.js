a78a50f45248a7e5afa7c4aff3446107fa856680
raghad2000 <rd.ah.sh1997@gmail.com>
2021-12-22 02:14:00 +0300

    update-frontend

---
+++ front-end/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const getData=()=>{
+     
+  }
+  return (
+    <div className="App">
+     <button onClick={getData}>Get Task</button>
+    </div>
+  );
+}
+
+export default App;
